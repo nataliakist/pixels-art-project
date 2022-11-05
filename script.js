5cfc3a399f0ac2fc5dd21210710d336642b96317
Natália Lauschner Kist <natalialkist@hotmail.com>
2022-11-05 18:40:45 -0300

    Resolvendo os problemas

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const randomColor = () => {
 	let r = parseInt(Math.random() * 255);
 	let g = parseInt(Math.random() * 255);
 	let b = parseInt(Math.random() * 255);
-	return `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const colors = document.querySelectorAll('.color')
+colors[0].style.backgroundColor = 'black';
+for(let i = 1; i < colors.length; i += 1){
+  colors[i].style.backgroundColor = randomColor();
 }
 
 const button = document.querySelector('#button-random-color')
-
 button.addEventListener('click', () => {
-	const colors = document.querySelectorAll('.color')
-	for(let i = 0; i < colors.length; i += 1){
-		if(i === 0){
-			colors[i].style.backgroundColor = 'black';
-		} else {
+	for(let i = 1; i < colors.length; i += 1){
 			colors[i].style.backgroundColor = randomColor();
 		}
-		}
-	});
+});
